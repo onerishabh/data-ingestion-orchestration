8e07291d22dcb8372585970e27f5138b2da313ca
Rishabh Srivastava <rishabh080598@gmail.com>
2022-07-12 14:19:53 +0530

    removing dead code

--- data_ingestion_infra/src/main/java/com/myorg/DataIngestionInfraApp.java
+++ data_ingestion_infra/src/main/java/com/myorg/DataIngestionInfraApp.java
@@ -1,10 +1,8 @@
 package com.myorg;
 
 import software.amazon.awscdk.App;
-// import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-// import java.util.Arrays;
 
 public class DataIngestionInfraApp {
     public static void main(final String[] args) {
