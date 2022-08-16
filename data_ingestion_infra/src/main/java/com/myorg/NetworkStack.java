9a0c2a50e48dc734c49235d6d3c2a7e7afd08b09
Rishabh Srivastava <rishabh080598@gmail.com>
2022-08-17 09:07:27 +1000

    made network attributes public

--- data_ingestion_infra/src/main/java/com/myorg/NetworkStack.java
+++ data_ingestion_infra/src/main/java/com/myorg/NetworkStack.java
@@ -1,6 +1,5 @@
 package com.myorg;
 
-
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -9,39 +8,41 @@ import software.amazon.awscdk.services.ec2.SecurityGroup;
 import software.amazon.awscdk.services.ec2.Port;
 import software.amazon.awscdk.CfnOutput;
 
-
 public class NetworkStack extends Stack {
+        public Vpc app_vpc;
+        public SecurityGroup msk_sg;
+        public SecurityGroup producer_lambda_sg;
 
-    public NetworkStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
+        public NetworkStack(final Construct scope, final String id) {
+                this(scope, id, null);
+        }
 
-    public NetworkStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+        public NetworkStack(final Construct scope, final String id, final StackProps props) {
+                super(scope, id, props);
 
-        final Vpc app_vpc = Vpc.Builder.create(this, "AppVPC")
-                .build();
-        
-        SecurityGroup msk_sg = SecurityGroup.Builder.create(this, "MSKSecurityGroup")
-                .allowAllOutbound(true)
-                .vpc(app_vpc)
-                .build();
-        
-        final SecurityGroup producer_lambda_sg = SecurityGroup.Builder.create(this, "ProducerLambdaSecurityGroup")
-                .allowAllOutbound(true)
-                .vpc(app_vpc)
-                .build();
+                this.app_vpc = Vpc.Builder.create(this, "AppVPC")
+                                .build();
 
-        msk_sg.getConnections().allowFrom(producer_lambda_sg, Port.allTraffic(), "LambdaToMSK");
+                this.msk_sg = SecurityGroup.Builder.create(this, "MSKSecurityGroup")
+                                .allowAllOutbound(true)
+                                .vpc(app_vpc)
+                                .build();
 
-        CfnOutput.Builder.create(this, "AppVPCId")
-                .value(app_vpc.getVpcId())
-                .build();
-        CfnOutput.Builder.create(this, "MSKSGId")
-                .value(msk_sg.getSecurityGroupId())
-                .build();
-        CfnOutput.Builder.create(this, "ProdcLambdaId")
-                .value(producer_lambda_sg.getSecurityGroupId())
-                .build();
-    }
+                this.producer_lambda_sg = SecurityGroup.Builder.create(this, "ProducerLambdaSecurityGroup")
+                                .allowAllOutbound(true)
+                                .vpc(app_vpc)
+                                .build();
+
+                this.msk_sg.getConnections().allowFrom(producer_lambda_sg, Port.allTraffic(), "LambdaToMSK");
+
+                CfnOutput.Builder.create(this, "AppVPCId")
+                                .value(this.app_vpc.getVpcId())
+                                .build();
+                CfnOutput.Builder.create(this, "MSKSGId")
+                                .value(this.msk_sg.getSecurityGroupId())
+                                .build();
+                CfnOutput.Builder.create(this, "ProdcLambdaId")
+                                .value(this.producer_lambda_sg.getSecurityGroupId())
+                                .build();
+        }
 }
